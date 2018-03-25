1a33ca4836e34b464ff4679451f2ad5fe239f0f7
yu-kadowaki <yu_kadowaki@r.recruit.co.jp>
2018-03-26 00:47:44 +0900

    Add Xcode project files & Carthage

---
+++ iOSSampleFramework/iOSSampleFramework.h
@@ -0,0 +1,19 @@
+//
+//  iOSSampleFramework.h
+//  iOSSampleFramework
+//
+//  Created by Yu Kadowaki on 2018/03/26.
+//  Copyright © 2018 Yu Kadowaki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSSampleFramework.
+FOUNDATION_EXPORT double iOSSampleFrameworkVersionNumber;
+
+//! Project version string for iOSSampleFramework.
+FOUNDATION_EXPORT const unsigned char iOSSampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSSampleFramework/PublicHeader.h>
+
+
